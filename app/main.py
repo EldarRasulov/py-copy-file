ee126ac337c4b030f5f5dc89a6f6484a87700a28
Eldar RasuLov <morddiff@gmail.com>
2026-07-08 00:22:08 +0400

    copy-file 0.2

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 def copy_file(command: str) -> None:
-    _, source_file, destination_file = command.split()
+    parts = command.split()
+
+    if len(parts) != 3:
+        return
+
+    operation, source_file, destination_file = parts
+
+    if operation != "cp":
+        return
 
     if source_file == destination_file:
         return
 
-    with open(source_file, "r", encoding="utf-8") as file_in, open(
-        destination_file,
-        "w",
-        encoding="utf-8",
-    ) as file_out:
-        file_out.write(file_in.read())
+    try:
+        with open(source_file, "r", encoding="utf-8") as file_in, open(
+            destination_file,
+            "w",
+            encoding="utf-8",
+        ) as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        return
